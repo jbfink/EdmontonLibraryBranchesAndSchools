7e43c58720cae379d4992ccaf68fda8f76e97549
Sam Popowich <spopowich@gmail.com>
2014-05-24 09:44:13 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EplHours::Application.config.secret_key_base = '6e3de40dbbdad194d61c28f78fe3fdabd04bb31033d01dcd679870a9f02537df549a9ebeaffbd41c1f63938e7dfff3590a786fd8ecb7c11288c98e622e328d30'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EplHours::Application.config.session_store :cookie_store, key: '_epl-hours_session'
