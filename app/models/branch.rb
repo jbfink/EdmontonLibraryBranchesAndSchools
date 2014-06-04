c672ca7b33b3eb0a69c9452bb6748e18490df95c
Sam Popowich <spopowich@gmail.com>
2014-06-04 10:53:03 -0700

    Fleshed out json-ld context.

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,10 +1,22 @@
 class Branch < ActiveRecord::Base
   include PragmaticContext::Contextualizable
 
-  #field :branch_name, :branch_id, :address, :phone
-
   contextualize :branch_name, :as => "http://schema.org/branch"
   contextualize :branch_id, :as => "http://example.com/id"
   contextualize :address, :as => "http://schema.org/address"
   contextualize :phone, :as => "http://example.com/phone"
+  contextualize :monday_open, :as => "http://example.com/time"
+  contextualize :monday_close, :as => "http://example.com/time"
+  contextualize :tuesday_open, :as => "http://example.com/time"
+  contextualize :tuesday_close, :as => "http://example.com/time"
+  contextualize :wednesday_open, :as => "http://example.com/time"
+  contextualize :wednesday_close, :as => "http://example.com/time"
+  contextualize :thursday_open, :as => "http://example.com/time"
+  contextualize :thursday_close, :as => "http://example.com/time"
+  contextualize :friday_open, :as => "http://example.com/time"
+  contextualize :friday_close, :as => "http://example.com/time"
+  contextualize :saturday_open, :as => "http://example.com/time"
+  contextualize :saturday_close, :as => "http://example.com/time"
+  contextualize :sunday_open, :as => "http://example.com/time"
+  contextualize :sunday_close, :as => "http://example.com/time"
 end
